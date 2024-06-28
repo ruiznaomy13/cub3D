015e872ad929154d68f91316246363e8cf3cb4aa
Nelly Naomy Castellanos Ruiz <ncastell@car10s6.42barcelona.com>
2024-06-28 19:37:13 +0200

    dimension updated line_ln

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: ncastell <ncastell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 20:00:44 by ncastell          #+#    #+#             */
-/*   Updated: 2024/06/28 16:08:21 by ncastell         ###   ########.fr       */
+/*   Updated: 2024/06/28 19:34:18 by ncastell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,9 +81,11 @@ int	read_dimension(int fd, t_map *map, char *map_file, int *map_row)
 	{
 		line_ln = line_length(line); // modificado hace POQUITPO
 		if (get_first_char(line) == '1')
+		{
 			map->rows += 1;
-		if (map->cols < line_ln)
-			map->cols = line_ln - 1;
+			if (map->cols < line_ln)
+				map->cols = line_ln - 1;
+		}
 		free(line);
 		line = get_next_line(fd);
 	}
